7517055ae1ff47314f201aa0923e7a15b66e5241
Todd Davis <tjdavis3@gmail.com>
2021-01-04 16:14:21 -0600

    Allow pulling of the list of template names

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/magna5/embedded.template
 
 go 1.15
 
-require github.com/GeertJohan/go.rice v1.0.2
+require (
+	github.com/GeertJohan/go.rice v1.0.2
+	github.com/daaku/go.zipexe v1.0.1 // indirect
+)
--- template.go
+++ template.go
@@ -9,18 +9,31 @@ import (
 	rice "github.com/GeertJohan/go.rice"
 )
 
-// Box is the `rice.Box` from which files can be loaded
-var Box *rice.Box
+type embTemplate struct {
+	Box   *rice.Box
+	Files []string
+}
+
+var embeddedTemplate = &embTemplate{}
 
 // LoadTemplates finds and parses all templates embedded in the application
 func LoadTemplates() (*texttmpl.Template, error) {
+	return embeddedTemplate.LoadTemplates()
+}
+
+func (embT *embTemplate) LoadTemplates() (*texttmpl.Template, error) {
+	var err error
 	config := &rice.Config{LocateOrder: []rice.LocateMethod{rice.LocateEmbedded, rice.LocateAppended, rice.LocateWorkingDirectory, rice.LocateFS}}
-	Box, err := config.FindBox("templates")
+	embT.Box, err = config.FindBox("templates")
 	if err != nil {
 		return nil, err
 	}
-	var files []string
-	Box.Walk("/", func(path string, info os.FileInfo, err error) error {
+	embT.Files = embT.listTemplates()
+	return embT.parseFiles(nil, embT.Files...)
+}
+
+func (embT *embTemplate) listTemplates() (files []string) {
+	embT.Box.Walk("/", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
 			return err
@@ -31,18 +44,22 @@ func LoadTemplates() (*texttmpl.Template, error) {
 		}
 		return nil
 	})
-	return parseFiles(nil, files...)
+	return files
+}
+
+func ListTemplates() []string {
+	return embeddedTemplate.Files
 }
 
 // parseFiles is the helper for the method and function. If the argument
 // template is nil, it is created from the first file.
-func parseFiles(t *texttmpl.Template, filenames ...string) (*texttmpl.Template, error) {
+func (embT *embTemplate) parseFiles(t *texttmpl.Template, filenames ...string) (*texttmpl.Template, error) {
 	if len(filenames) == 0 {
 		// Not really a problem, but be consistent.
 		return nil, fmt.Errorf("template: no files named in call to ParseFiles")
 	}
 	for _, filename := range filenames {
-		b, err := Box.Bytes(filename)
+		b, err := embT.Box.Bytes(filename)
 		if err != nil {
 			return nil, err
 		}
